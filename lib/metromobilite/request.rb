a61ec8de69434b0a514d525494d8c52805051a11
Léo GATELLIER <git@leogatellier.fr>
2021-10-17 20:33:37 +0200

    fix(requests): add default HTTP header Origin: rubygems.org

--- lib/metromobilite/request.rb
+++ lib/metromobilite/request.rb
@@ -3,11 +3,12 @@ require 'json'
 
 module Metromobilite
     module Request
-        API_URI = 'http://data.metromobilite.fr/api'
-        
+        API_URI = 'http://data.mobilites-m.fr/api'
+        HEADERS = { 'Origin' => 'rubygems.org' }
+
         def self.get(reqUri)
             puts 'Calling GET on ' + API_URI + reqUri
-            httpResult = HTTParty.get(API_URI + reqUri)
+            httpResult = HTTParty.get(API_URI + reqUri, headers: HEADERS)
             JSON.parse(httpResult.body)
         end
     end
